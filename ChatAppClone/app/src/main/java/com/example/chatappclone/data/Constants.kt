b338bbda064c5e08be66a0829c0dced291d506f8
Ozgur-inci <inciozgur462@gmail.com>
2024-09-11 16:23:34 +0300

    Chat_App
    Kotlin kullaanarak hazırlanan chat uygulaması.

---
+++ ChatAppClone/app/src/main/java/com/example/chatappclone/data/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.chatappclone.data
+
+// Firebase'de koleksiyon oluşturma
+
+const val COLLECTION_USER="user"
+const val COLLECTION_CHAT="chat"
+const val COLLECTION_MESSAGE="messages"
+const val COLLECTION_STATUS="status"
---
+++ ChatAppClone/app/src/main/java/com/example/chatappclone/data/Event.kt
@@ -0,0 +1,19 @@
+package com.example.chatappclone.data
+
+class Event<out T>(private val content:T) {
+    var hasBeenHandled=false
+    private set
+
+
+    fun getContentOrNull(): T?{
+
+        return if (hasBeenHandled)
+            null
+        else{
+            hasBeenHandled=true
+            content
+        }
+
+
+    }
+}
